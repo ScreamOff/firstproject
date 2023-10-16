12d44537df44b133cd93b70fb92d8b4509a7c1f3
maciek <shadoow.clank@interia.pl>
2023-10-16 21:11:15 +0200

    karty prosciej

--- src/Card.java
+++ src/Card.java
@@ -1,42 +1,24 @@
-//klasa karty majaca reprezentowac karte do gry
-
-
-public class Card {
-    private final Ranga ranga;
-    private final Kolor kolor;
-
-
-    public enum Ranga{
-        ACE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(10), QUEEN(10), KING(10);
-        private int wartosc;
-
-        Ranga(int wartosc){//konstruktor dla rangi
-            this.wartosc = wartosc;
-        }
-        @Override
-        public String toString(){//nadpisana metoda na zwracanie rangi karty w formie string
-            return this.name().charAt(0) + this.name().substring(1, this.name().length()).toLowerCase();
-        }
-    }
-    public enum Kolor{
-        CLUBS, DIAMONDS, HEARTS, SPADES;
-        @Override
-        public String toString() {//nadpisana metoda na zwracanie koloru karty w formie string
-            return this.name().charAt(0) + this.name().substring(1, this.name().length()).toLowerCase();
-    }
-
-}
-    public Card(Ranga ranga,Kolor kolor){//konstruktor dla calej karty
-        this.kolor = kolor;
+public class Card{
+    private String kolor;
+    private String ranga;
+    private String pathToPng;
+    public Card(String ranga,String kolor){
         this.ranga = ranga;
-
+        this.kolor = kolor;
+        this.pathToPng= "cards/" + ranga.toLowerCase() + "_of_" + kolor.toLowerCase() + ".png";
     }
-    public int wartosc(){//zwraca wartosc rangi z obiektu karty
-        return this.ranga.wartosc;
+    public String getRanga(){
+        return ranga;
+    }
+    public String getKolor(){
+        return kolor;
+    }
+    public String getPathToPng(){
+        return pathToPng;
     }
     @Override
-    public String toString() {//nadpisana metoda klasy enum zwracajaca calosc karty w jezyku angielskim
-        return this.ranga + " of " + this.kolor;
+    public String toString(){
+        return ranga.toLowerCase() + "_of_" + kolor.toLowerCase();
     }
 
 }
