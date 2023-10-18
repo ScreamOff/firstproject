94b47f0135a72f91a651897fc537f46866966e73
maciek <shadoow.clank@interia.pl>
2023-10-18 16:14:09 +0200

    deck

--- src/Deck.java
+++ src/Deck.java
@@ -2,38 +2,46 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Stack;
+
 public class Deck {
     private Stack<Card> cards;
 
-    public Deck(){
+    public Deck() {
         cards = new Stack<>();
         initializeDeck();
     }
-    private void initializeDeck(){
-        List<Card> cardList = new ArrayList<>();
 
-        String[] rangi = {"two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "Jack", "Queen", "King", "Ace"};
-        String[] kolory = {"Hearts", "Diamonds", "Clubs", "Spades"};
-        for(String kolor : kolory){
-            for(String ranga:rangi){
-                Card card = new Card(ranga,kolor);
+    private void initializeDeck() {
+        List<Card> cardList = new ArrayList<>(); // Tworzymy listę kart
+
+        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+
+        for (String suit : suits) {
+            for (String rank : ranks) {
+                Card card = new Card(rank, suit);
                 cardList.add(card);
             }
-
         }
+
+        // Tasujemy listę kart
         Collections.shuffle(cardList);
-        for(Card card:cardList){
+
+        // Przenosimy karty z listy do stosu
+        for (Card card : cardList) {
             cards.push(card);
         }
     }
-    public Card drawCard(){
-        if(!cards.isEmpty()){
+
+    public Card drawCard() {
+        if (!cards.isEmpty()) {
             return cards.pop();
-        }else{
-            return null;
+        } else {
+            return null; // No cards left in the deck
         }
     }
-    public int getDeckSize(){
+
+    public int getDeckSize() {
         return cards.size();
     }
 }
